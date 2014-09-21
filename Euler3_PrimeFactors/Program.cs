9a12268cce451cf284d9f94b9d1f79ad29d94061
nm <neil.mccoy@outlook.com>
2014-09-21 21:19:21 +1000

    Euler3 Prime Factors test setup

---
+++ Euler3_PrimeFactors/Program.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+
+namespace Euler3_PrimeFactors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    [TestFixture]
+    public class FactorsTest
+    {
+        [Test]
+        public void CanTest()
+        {
+            
+        }
+    }
+}
