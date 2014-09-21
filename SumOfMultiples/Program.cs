b1d1ec6158f820af1af37f850d6b9faaba220f34
nm <neil.mccoy@outlook.com>
2014-09-21 17:59:13 +1000

    Multiples State Tests

--- SumOfMultiples/Program.cs
+++ SumOfMultiples/Program.cs
@@ -43,10 +43,13 @@ namespace SumOfMultiples
     public class GetMultiplesTest
     {
         [Test]
-        public void GetMultiplesOfOneUpToTen()
+        public void MultiplesStateTests()
         {
-            TestMultipliers(1, 10, new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 });
-            TestMultipliers(2, 10, new[] { 2, 4, 6, 8, 10 });
+            TestMultipliers(1, 10, new[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10});
+            TestMultipliers(2, 10, new[] {2, 4, 6, 8, 10});
+            TestMultipliers(3, 10, new[] {3, 6, 9});
+            TestMultipliers(4, 20, new[] {4, 8, 12, 16, 20});
+            TestMultipliers(5, 30, new[] {5, 10, 15, 20, 25, 30});
         }
 
         private static void TestMultipliers(int multiplier, int max, IEnumerable<int> expected)
