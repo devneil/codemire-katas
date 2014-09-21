0a5f70dc82592268eefb24f2e795a682dbeefe77
nm <neil.mccoy@outlook.com>
2014-09-21 21:22:46 +1000

    Empty on one test

--- Euler3_PrimeFactors/Program.cs
+++ Euler3_PrimeFactors/Program.cs
@@ -1,7 +1,12 @@
-﻿using NUnit.Framework;
+﻿using FluentAssertions;
+using NUnit.Framework;
 
 namespace Euler3_PrimeFactors
 {
+    /// <summary>
+    /// The prime factors of 13195 are 5, 7, 13 and 29.
+    /// What is the largest prime factor of the number 600851475143 ?
+    /// </summary>
     class Program
     {
         static void Main(string[] args)
@@ -13,9 +18,18 @@ namespace Euler3_PrimeFactors
     public class FactorsTest
     {
         [Test]
-        public void CanTest()
+        public void OneReturnsEmpty()
         {
-            
+            int[] factors = new Factors().GetPrimeFactorsOf(1);
+            factors.Should().BeEmpty();
+        }
+    }
+
+    public class Factors
+    {
+        public int[] GetPrimeFactorsOf(int thisNumber)
+        {
+            return new int[0];
         }
     }
 }
