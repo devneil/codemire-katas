8ec28523e3d74af5d33cdc01133b9d83ec283ed3
nm <neil.mccoy@outlook.com>
2014-09-21 18:11:40 +1000

    Fixing requirement mis read :: Less than number, bot up to number

--- SumOfMultiples/Program.cs
+++ SumOfMultiples/Program.cs
@@ -27,14 +27,19 @@ namespace SumOfMultiples
         {
             TestSumMultiples(1, 0);
             TestSumMultiples(2, 0);
-            TestSumMultiples(3, 3);
+            TestSumMultiples(3, 0);
             TestSumMultiples(4, 3);
-            TestSumMultiples(5, 8);
+            TestSumMultiples(5, 3);
+            TestSumMultiples(6, 8);
+            TestSumMultiples(7, 14);
+            TestSumMultiples(8, 14);
+            TestSumMultiples(9, 14);
+            TestSumMultiples(10, 23);
         }
 
-        private static void TestSumMultiples(int max, int expected)
+        private static void TestSumMultiples(int lessThan, int expected)
         {
-            int result = new SumMultiples().UpTo(max);
+            int result = new SumMultiples().UpTo(lessThan - 1);
             result.Should().Be(expected);
         }
     }
