9935973970f4706617027ddb84686a33b58809f5
nm <neil.mccoy@outlook.com>
2014-09-21 18:06:05 +1000

    Refactor SumMultiples to use get multiples

---
+++ SumOfMultiples/GetMultiplesOf.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+
+namespace SumOfMultiples
+{
+    public class GetMultiplesOf
+    {
+        private readonly int _multiplier;
+
+        public GetMultiplesOf(int multiplier)
+        {
+            _multiplier = multiplier;
+        }
+
+        public int[] UpTo(int max)
+        {
+            var returnVal = new List<int>();
+            for (int i = 1; i <= max; i++)
+            {
+                if (IsDivisibleBy(i, _multiplier))
+                {
+                    returnVal.Add(i);
+                }
+            }
+            return returnVal.ToArray();
+        }
+
+        private static bool IsDivisibleBy(int dividend, int divisor)
+        {
+            return dividend%divisor == 0;
+        }
+    }
+}
--- SumOfMultiples/Program.cs
+++ SumOfMultiples/Program.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections.Generic;
+﻿using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using FluentAssertions;
 using NUnit.Framework;
@@ -39,65 +39,11 @@ namespace SumOfMultiples
         }
     }
 
-    [TestFixture]
-    public class GetMultiplesTest
-    {
-        [Test]
-        public void MultiplesStateTests()
-        {
-            TestMultipliers(1, 10, new[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10});
-            TestMultipliers(2, 10, new[] {2, 4, 6, 8, 10});
-            TestMultipliers(3, 10, new[] {3, 6, 9});
-            TestMultipliers(4, 20, new[] {4, 8, 12, 16, 20});
-            TestMultipliers(5, 30, new[] {5, 10, 15, 20, 25, 30});
-        }
-
-        private static void TestMultipliers(int multiplier, int max, IEnumerable<int> expected)
-        {
-            int[] result = new GetMultiplesOf(multiplier).UpTo(max);
-            result.Should().ContainInOrder(expected);
-        }
-
-    }
-
-    public class GetMultiplesOf
-    {
-        private readonly int _multiplier;
-
-        public GetMultiplesOf(int multiplier)
-        {
-            _multiplier = multiplier;
-        }
-
-        public int[] UpTo(int max)
-        {
-            var returnVal = new List<int>();
-            for (int i = 1; i <= max; i++)
-            {
-                if (IsDivisibleBy(i, _multiplier))
-                {
-                    returnVal.Add(i);
-                }
-            }
-            return returnVal.ToArray();
-        }
-
-        private static bool IsDivisibleBy(int dividend, int divisor)
-        {
-            return dividend%divisor == 0;
-        }
-    }
-
     public class SumMultiples
     {
         public int UpTo(int max)
         {
-            if (max >= 3)
-            {
-                return 3;
-            }
-            return 0;
-
+            return new GetMultiplesOf(3).UpTo(max).Sum();
         }
     }
 }
