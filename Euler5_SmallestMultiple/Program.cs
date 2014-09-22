2415a82940709648150fcb431b5591563f2b8539
nm <neil.mccoy@outlook.com>
2014-09-22 22:10:12 +1000

    Euler5 - smallest multiple

---
+++ Euler5_SmallestMultiple/Program.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+
+namespace Euler5_SmallestMultiple
+{
+    /// <summary>
+    /// 2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+    /// What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    [TestFixture]
+    public class FactorsTest
+    {
+        [Test]
+        public void CanTest()
+        {
+            
+        }
+    }
+
+}
