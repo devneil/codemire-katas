4bfcb1d5fa9bc2ba97509ffe2ec40f9deb4c17b9
nm <neil.mccoy@outlook.com>
2014-09-23 20:45:17 +1000

    Euler7

---
+++ Euler7_Prime/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Euler7_Prime
+{
+    class Program
+    {
+        /// <summary>
+        /// By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
+        /// What is the 10 001st prime number?
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+        }
+
+        [TestFixture]
+        public class PrimeNumberGeneratorTest
+        {
+            [Test]
+            public void CanTest()
+            {
+                
+            }
+        }
+    }
+}
