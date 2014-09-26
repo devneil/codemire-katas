2c3f24ab0084762286c744a783fad856385529f8
nm <neil.mccoy@outlook.com>
2014-09-26 22:34:19 +1000

    PRogram complete

--- Euler9_SpecialPythagoreanTriplet/Program.cs
+++ Euler9_SpecialPythagoreanTriplet/Program.cs
@@ -1,4 +1,8 @@
-﻿using NUnit.Framework;
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using FluentAssertions;
+using NUnit.Framework;
 
 namespace Euler9_SpecialPythagoreanTriplet
 {
@@ -15,16 +19,91 @@ namespace Euler9_SpecialPythagoreanTriplet
         /// <param name="args"></param>
         static void Main(string[] args)
         {
+            var result = GetAbc();
+            Console.WriteLine("The result is: {0}", result);
+            Console.ReadLine();
         }
 
-        [TestFixture]
-        public class TestClass
+        private static int GetAbc()
         {
-            [Test]
-            public void CanTest()
+            var numbersWithNaturalSquares = new List<int>();
+            var sq = new SquareRooter();
+
+            for (int i = 1; i < 1000; i++)
             {
-                
+                numbersWithNaturalSquares.Add(i*i);
             }
+
+            for (int i = 0; i < numbersWithNaturalSquares.Count; i++)
+            {
+                for (int j = 0; j < numbersWithNaturalSquares.Count; j++)
+                {
+                    var a2 = numbersWithNaturalSquares[i];
+                    var b2 = numbersWithNaturalSquares[j];
+                    int c2 = a2 + b2;
+                    if (numbersWithNaturalSquares.Contains(c2))
+                    {
+                        var a = sq.GetSquareRoot(a2);
+                        var b = sq.GetSquareRoot(b2);
+                        var c = sq.GetSquareRoot(c2);
+                        if (a + b + c == 1000)
+                        {
+                            return a*b*c;
+                        }
+                    }
+                }
+            }
+            return 0;
+        }
+    }
+    
+    [TestFixture]
+    public class TestSquareRoots
+    {
+        [Test]
+        public void SquareTests()
+        {
+            TestIsNaturalSquare(4);
+            TestIsNotNaturalSquare(5);
+            TestIsNaturalSquare(16);
+            TestIsNaturalSquare(25);
+            TestIsNaturalSquare(36);
+            TestIsNotNaturalSquare(39);
+        }
+        
+        private static void TestIsNotNaturalSquare(int num)
+        {
+            GetIsNatural(num).Should().BeFalse();
+        }
+
+        private static void TestIsNaturalSquare(int num)
+        {
+            GetIsNatural(num).Should().BeTrue();
+        }
+
+
+        private static bool GetIsNatural(int num)
+        {
+            return new SquareRooter().IsNatural(num);
+        }
+    }
+
+    internal class SquareRooter
+    {
+        public bool IsNatural(int num)
+        {
+            int dummy;
+            return int.TryParse(Math.Sqrt(num).ToString(CultureInfo.InvariantCulture), out dummy);
+        }
+
+        public int GetSquareRoot(int num)
+        {
+            int result;
+            if (int.TryParse(Math.Sqrt(num).ToString(CultureInfo.InvariantCulture), out result))
+            {
+                return result;
+            }
+            return 0;
         }
     }
 }
